cf865530b56f71b833d822e96683b83189c65144
jrhoads <joseph_rhoads@brown.edu>
2018-07-26 12:56:30 -0400

    Allow multicore/parallel downloads

--- mods_download.py
+++ mods_download.py
@@ -1,15 +1,19 @@
-from __future__ import print_function
-import requests
-import os
-import datetime
 import begin
 import collections
+import datetime
+import requests
+import os
+
+
+from pathos.pools import ProcessPool as Pool
+from functools import partial
 from tqdm import tqdm
 
 DEFAULT_SOLR_URL = "https://repository.library.brown.edu/api/search/"
 MODS_URL_PATTERN = 'https://repository.library.brown.edu/storage/{pid}/MODS/'
 DEFAULT_QUERY = "NOTHING"
 DEFAULT_BASE_DIR = "./downloaded"
+DEFAULT_PROCESSES = 4
 ROWS = 500
 
 
@@ -33,6 +37,34 @@ class Mods_Doc(collections.namedtuple("Doc", ['pid'])):
     @property
     def uri(self):
         return MODS_URL_PATTERN.format(pid=self.pid)
+
+    def save(self, save_dir=DEFAULT_BASE_DIR):
+        download_file(self.url, self.filename, base_dir=save_dir)
+
+
+class DocSaver:
+    def __init__(self, base_dir):
+        self.base_dir = base_dir
+        self._save_dir=self.setup_storage()
+
+    @property
+    def save_dir(self):
+        if not self._save_dir:
+            self._save_dir = self.setup_storage()
+        return self._save_dir
+
+    def setup_storage(self):
+        base_dir = self.base_dir
+        if not os.path.exists(base_dir):
+            os.makedirs(base_dir)
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        storage_directory = os.path.join(base_dir, timestamp)
+        if not os.path.exists(storage_directory):
+            os.makedirs(storage_directory)
+        return storage_directory
+
+    def save(self, doc):
+        download_file(doc.url, doc.filename, self.save_dir)
 
 
 def get_solr_docs(query=DEFAULT_QUERY, start=0, solr_url=DEFAULT_SOLR_URL):
@@ -59,22 +91,23 @@ def get_solr_docs(query=DEFAULT_QUERY, start=0, solr_url=DEFAULT_SOLR_URL):
             )
     return docs
 
-def setup_storage(base_dir):
-    if not os.path.exists(base_dir):
-        os.makedirs(base_dir)
-    timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    storage_directory = os.path.join(base_dir, timestamp)
-    if not os.path.exists(storage_directory):
-        os.makedirs(storage_directory)
-    return storage_directory
 
+
+def download_doc(doc, save_dir):
+    doc.save(save_dir)
 
 @begin.start(auto_convert=True)
 def main(
         query=DEFAULT_QUERY,
+        number_of_processes=DEFAULT_PROCESSES,
         base_dir=DEFAULT_BASE_DIR):
-    storage_directory = setup_storage(base_dir)
     docs = get_solr_docs(query)
-    for d in tqdm(docs):
-        doc = Mods_Doc(**d)
-        download_file(doc.url, doc.filename, storage_directory)
+    saver = DocSaver(base_dir)
+    pool = Pool(number_of_processes)
+    mdocs = (Mods_Doc(**d) for d in docs)
+    max_= len(docs)
+    with tqdm(total=max_) as pbar:
+        for i, _ in tqdm(enumerate(pool.imap(saver.save, mdocs))):
+            pbar.update()
+    pool.close()
+    pool.join()
